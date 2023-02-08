bb1277f7af6ab1b83b8cc24b7e11fbe25d86cda2
Christian Wander <cwshit@posteo.de>
2023-02-08 13:10:53 +0100

    initial commit

---
+++ src/main/java/org/acme/Product.java
@@ -0,0 +1,73 @@
+package org.acme;
+
+import io.quarkus.hibernate.reactive.panache.Panache;
+import io.quarkus.hibernate.reactive.panache.PanacheEntityBase;
+import io.smallrye.mutiny.Uni;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.json.bind.annotation.JsonbDateFormat;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.ZonedDateTime;
+
+@Entity
+public class Product extends PanacheEntityBase {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Long id;
+
+    public String title;
+
+    public String description;
+
+    @CreationTimestamp
+    @JsonbDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'")
+    public ZonedDateTime createdAt;
+
+    @UpdateTimestamp
+    @JsonbDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'")
+    public ZonedDateTime updatedAt;
+
+    // private static final Logger LOG = Logger.getLogger(Product.class);
+    private static final Logger LOG = LoggerFactory.getLogger(Product.class);
+
+    public static Uni<Product> findByProductId(Long id) {
+        return findById(id);
+    }
+
+    public static Uni<Product> addProduct(Product product) {
+        return Panache
+                .withTransaction(product::persist)
+                .replaceWith(product);
+
+    }
+
+    public static Uni<Product> updateProduct(Long id, Product product) {
+
+        return Panache.withTransaction(() -> Product.findByProductId(id)
+                .onItem().ifNotNull()
+                .transform( entity -> {
+                    entity.title = product.title;
+                    entity.description = product.description;
+                    return entity;
+                })
+                .onFailure().recoverWithNull());
+
+    }
+
+    public static Uni<Boolean> deleteProduct(Long id) {
+
+        LOG.debug("Request to delete product {}", id);
+
+        return Panache.withTransaction(() -> deleteById(id));
+
+    }
+
+
+}
---
+++ src/main/java/org/acme/ProductResource.java
@@ -0,0 +1,65 @@
+package org.acme;
+
+import io.quarkus.panache.common.Sort;
+import io.smallrye.mutiny.Uni;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.Response;
+import java.net.URI;
+import java.util.List;
+
+import static javax.ws.rs.core.Response.Status.NOT_FOUND;
+import static javax.ws.rs.core.Response.Status.NOT_IMPLEMENTED;
+
+@Path("/products")
+public class ProductResource {
+
+    @GET
+    public Uni<List<Product>> getProducts() {
+
+        return Product.listAll(Sort.by("createdAt"));
+    }
+
+    @GET
+    @Path("{id}")
+    public Uni<Response> getSingleProduct(Long id) {
+
+        return Product.findByProductId(id)
+                .onItem().ifNotNull().transform(p -> Response.ok(p).build())
+                .onItem().ifNull().continueWith(Response.ok().status(404).build());
+
+    }
+
+    @POST
+    public Uni<Response> addProduct(Product product) {
+        return Product.addProduct(product)
+                .onItem().transform(p -> URI.create("/products/" + p.id))
+                .onItem().transform(uri -> Response.created(uri).build());
+    }
+
+    @PUT
+    @Path("{id}")
+    public Uni<Response> updateProduct(Long id, Product product) {
+        return Product.updateProduct(id, product)
+                .onItem().ifNotNull()
+                .transform(p -> Response.ok(p).build())
+                .onItem().ifNull()
+                .continueWith(Response.ok().status(404).build());
+    }
+
+    @DELETE
+    @Path("{id}")
+    public Uni<Response> delete(Long id) {
+
+        return Product.deleteProduct(id).onItem().transform( deleted -> {
+            return deleted ? Response.ok().build()
+                    : Response.serverError().status(NOT_FOUND).build();
+        });
+
+    }
+
+    private static Uni<Response> notImplemented() {
+        return Uni.createFrom().item(Response.serverError().status(NOT_IMPLEMENTED).build());
+    }
+
+}
